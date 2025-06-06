2a1f70c65861a5b5bbf663d94e3f84b6a4cabadd
Aaron Boodman <aaron@aaronboodman.com>
2025-06-05 17:26:40 -1000

    remove virtual scroll

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,7 @@
 import {useQuery, useZero} from '@rocicorp/zero/react';
 import {type Schema} from '../../zero/schema';
 import {createFileRoute} from '@tanstack/react-router';
-import {useWindowVirtualizer} from '@tanstack/react-virtual';
-import {CSSProperties, useEffect, useRef, useState} from 'react';
+import {useState} from 'react';
 
 export const Route = createFileRoute('/')({
   component: Home,
@@ -11,52 +10,14 @@ export const Route = createFileRoute('/')({
 
 function Home() {
   const z = useZero<Schema>();
-  const itemSize = 21;
-
   const [search, setSearch] = useState('');
 
-  let q = z.query.artist.related('albums').orderBy('name', 'asc');
+  let q = z.query.artist.related('albums').orderBy('name', 'asc').limit(100);
   if (search) {
     q = q.where('name', 'ILIKE', `%${search}%`);
   }
 
-  const pageSize = Math.ceil(window.innerHeight / itemSize);
-  const [limit, setLimit] = useState(pageSize);
-  useEffect(() => {
-    setLimit(pageSize);
-    virtualizer.scrollToIndex(0);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [q.hash()]);
-
-  q = q.limit(limit);
-
   const [artists] = useQuery(q);
-  const listRef = useRef<HTMLUListElement | null>(null);
-
-  const virtualizer = useWindowVirtualizer({
-    count: artists.length,
-    estimateSize: () => itemSize,
-    overscan: 0,
-    scrollMargin: 0,
-    getItemKey: index => artists[index].id,
-  });
-  const virtualItems = virtualizer.getVirtualItems();
-  useEffect(() => {
-    const [lastItem] = virtualItems.reverse();
-    if (!lastItem) {
-      return;
-    }
-
-    if (lastItem.index >= limit - pageSize / 4) {
-      setLimit(limit + pageSize);
-    }
-  }, [limit, virtualItems]);
-
-  const ArtistRow = ({index, style}: {index: number; style: CSSProperties}) => {
-    return (
-      <li style={{...style, position: 'absolute'}}>{artists[index].name}</li>
-    );
-  };
 
   return (
     <>
@@ -65,18 +26,9 @@ function Home() {
         value={search}
         onChange={e => setSearch(e.target.value)}
       />
-      <ul
-        ref={listRef}
-        style={{position: 'relative', listStyle: 'none', padding: 0}}
-      >
-        {virtualItems.map(virtualRow => (
-          <ArtistRow
-            key={virtualRow.key}
-            index={virtualRow.index}
-            style={{
-              transform: `translateY(${virtualRow.start}px)`,
-            }}
-          />
+      <ul>
+        {artists.map(artist => (
+          <li key={artist.id}>{artist.name}</li>
         ))}
       </ul>
     </>
