9f956b7d9422e8bd11b8adc17f263414dcd8d4c8
Aaron Boodman <aaron@aaronboodman.com>
2025-06-07 17:55:12 -1000

    Create correct Zero instance

--- app/client.tsx
+++ app/client.tsx
@@ -3,69 +3,13 @@
 import {hydrateRoot} from 'react-dom/client';
 import {StartClient} from '@tanstack/react-start';
 import {createRouter} from './router';
-import {dropAllDatabases, Zero} from '@rocicorp/zero';
-import {schema} from '../zero/schema';
-import {ZeroProvider} from '@rocicorp/zero/react';
+import {ZeroProvider} from './components/zero-provider';
 
 const router = createRouter();
 
-const z = new Zero({
-  userID: 'anon',
-  server: import.meta.env.VITE_PUBLIC_SERVER,
-  schema,
-});
-
-(window as any).__zero = z;
-(window as any).__inspector = await z.inspect();
-
-// TODO: OK this is not great 😅 and will be improved.
-//
-// Couple things going on:
-//
-// 1. Despite the name, we don't want "preload" queries to generally block the
-//    queries made by the UI. We need better nomenclature here maybe. But
-//    generally I think these queries should be lower-priority and allow the
-//    data the UI actually needs to get synced first.
-//
-// 2. We have found that setting a long ttl is not a good idea for most apps,
-//    because as the app changes, the developer changes the queries they want,
-//    but we keep syncing old queries with long ttls anyway even though the
-//    app doesn't need them.
-//
-//    Anyway, the way ttl works is it only comes into play when a query is *not*
-//    active (when a window is running that does *not* include the query). So
-//    for the common preload case the ttl doesn't need to be long, because the
-//    preload query is always running. Basically it just needs to be long enough
-//    so that the query doesn't accidentally get evicted during page navigation.
-//    A minute is plenty.
-//
-// We are going to rework the ttl API to take both of these into account, but
-// for now we do not recomend ttls longer than 1h, and for preload we only
-// recommend 1m.
-window.setTimeout(() => {
-  // Why this particular preload?
-  //
-  // Well it's a value judgement. This UI sorts by name and has a search box.
-  // The main way users will be interacting with this data is via search, so we
-  // probably want at least the artist names available.
-  //
-  // Basically we are trying to optimize the chance that something the user will
-  // do will respond instantly. Since the UI doesn't allow accessing albums
-  // immediately we don't need that.
-  //
-  // The artist data is about 10MB uncompressed so if we were dealing with more
-  // data, or if we wanted search to work over album titles too, we might want
-  // to reconsider this. Perhaps in that case we'd preload a prefix of artists
-  // and albums sorted by popularity, so that when you search, popular results
-  // will show up first.
-  z.query.artist.orderBy('popularity', 'desc').limit(1_000).preload({
-    ttl: '1m',
-  });
-}, 1000);
-
 hydrateRoot(
   document,
-  <ZeroProvider zero={z}>
+  <ZeroProvider>
     <StartClient router={router} />
   </ZeroProvider>,
 );
---
+++ app/components/zero-provider.tsx
@@ -0,0 +1,107 @@
+import {Zero} from '@rocicorp/zero';
+import {schema, Schema} from '../../zero/schema';
+import {ZeroProvider as ZeroProviderImpl} from '@rocicorp/zero/react';
+import {authClient} from '../../auth/client';
+import {useEffect, useState} from 'react';
+
+export function ZeroProvider({children}: {children: React.ReactNode}) {
+  const session = authClient.useSession();
+
+  const [zero, setZero] = useState<Zero<Schema> | null>(null);
+
+  useEffect(() => {
+    // Don't want to create an anon zero client, then immediately create a
+    // auth'd one instead, so wait for session to resolve.
+    if (session.isPending) {
+      return;
+    }
+
+    const z = new Zero({
+      userID: session.data?.user.id ?? 'anon',
+      server: import.meta.env.VITE_PUBLIC_SERVER,
+      schema,
+    });
+
+    setZero(z);
+    preload(z);
+
+    const deinstrument = instrument(z);
+
+    return () => {
+      zero?.close();
+      setZero(null);
+      deinstrument();
+    };
+  }, [session.data?.user.id]);
+
+  if (!zero) {
+    return null;
+  }
+
+  return <ZeroProviderImpl zero={zero}>{children}</ZeroProviderImpl>;
+}
+
+function instrument(z: Zero<Schema>) {
+  (window as any).__zero = z;
+  z.inspect().then(inspector => {
+    (window as any).__inspector = inspector;
+  });
+
+  return () => {
+    (window as any).__zero = undefined;
+    (window as any).__inspector = undefined;
+  };
+}
+
+function preload(z: Zero<Schema>) {
+  // TODO: OK this is not great 😅 and will be improved.
+  //
+  // Couple things going on:
+  //
+  // 1. Despite the name, we don't want "preload" queries to generally block the
+  //    queries made by the UI. We need better nomenclature here maybe. But
+  //    generally I think these queries should be lower-priority and allow the
+  //    data the UI actually needs to get synced first.
+  //
+  // 2. We have found that setting a long ttl is not a good idea for most apps,
+  //    because as the app changes, the developer changes the queries they want,
+  //    but we keep syncing old queries with long ttls anyway even though the
+  //    app doesn't need them.
+  //
+  //    Anyway, the way ttl works is it only comes into play when a query is *not*
+  //    active (when a window is running that does *not* include the query). So
+  //    for the common preload case the ttl doesn't need to be long, because the
+  //    preload query is always running. Basically it just needs to be long enough
+  //    so that the query doesn't accidentally get evicted during page navigation.
+  //    A minute is plenty.
+  //
+  // We are going to rework the ttl API to take both of these into account, but
+  // for now we do not recomend ttls longer than 1h, and for preload we only
+  // recommend 1m.
+  setTimeout(() => {
+    // Why this particular preload?
+    //
+    // The goal of Zero generally is for every user interaction to be instant.
+    // This relies fundamentally on preloading data. But we cannot preload
+    // everything, so preloading is at core about guessing data user will most
+    // likely need. This is different in every app, and Zero gives you the full
+    // power of queries to express it.
+    //
+    // For this app, the primary interface is a search box. Users are more
+    // likely to search for popular artists than unpopular so we preload the
+    // first 1k artists by popularity.
+    //
+    // Note that we don't also preload their albums. We could, but there's no
+    // reason to as the list UI will do that, and we know the user can't nav
+    // to an album they don't see in the UI.
+    //
+    // There is also an interesting interaction with the UI. Since we will get
+    // instant local results and full server results async, we ideally want to
+    // avoid having the UI jostle. So we want to preload in the same order we
+    // tend to display in the UI. That way local results are always also the
+    // top ranked results.
+    z.query.artist.orderBy('popularity', 'desc').limit(1_000).preload({
+      ttl: '1m',
+    });
+  }, 1000);
+}
