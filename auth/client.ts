ccbd00991a558a2c8267c64b3c4ea82f0c310c34
Aaron Boodman <aaron@aaronboodman.com>
2025-06-11 20:37:24 -1000

    use location.origin

--- auth/client.ts
+++ auth/client.ts
@@ -1,9 +1,5 @@
 import { createAuthClient } from "better-auth/react"
-import { must } from "../shared/must"
-import "../shared/env"
-
-const baseURL = must(process.env.BETTER_AUTH_URL, "BETTER_AUTH_URL is required")
 
 export const authClient = createAuthClient({
-  baseURL,
+  baseURL: window.location.origin,
 });
