9debd02dbaf8e0401b65a19c850089a38a68b36a
Aaron Boodman <aaron@aaronboodman.com>
2025-06-11 20:26:14 -1000

    fix env more

--- auth/client.ts
+++ auth/client.ts
@@ -1,4 +1,9 @@
 import { createAuthClient } from "better-auth/react"
+import { must } from "../shared/must"
+import "../shared/env"
+
+const baseURL = must(process.env.BETTER_AUTH_URL, "BETTER_AUTH_URL is required")
+
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000"
+  baseURL,
 });
