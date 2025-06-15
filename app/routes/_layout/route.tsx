c3a0a7b29f0d0a01c3ec5b2f8da40673b3a72d28
Aaron Boodman <aaron@aaronboodman.com>
2025-06-15 01:06:05 -1000

    SSR auth

--- app/routes/_layout/route.tsx
+++ app/routes/_layout/route.tsx
@@ -1,25 +1,55 @@
 import {createFileRoute, Outlet} from '@tanstack/react-router';
 import {SessionProvider} from '../../components/session-provider';
-import {authClient} from '../../../auth/client';
+import {auth} from '../../../auth/auth';
 import {ZeroProvider} from '../../components/zero-provider';
+import {getWebRequest} from '@tanstack/react-start/server';
+import {createServerFn} from '@tanstack/react-start';
+
+export const getAuthFromHeaders = createServerFn().handler(async () => {
+  const session = await auth.api.getSession({
+    headers: getWebRequest().headers,
+  });
+
+  const token = await auth.api.getToken({
+    headers: getWebRequest().headers,
+  });
+
+  return {
+    userID: session?.user.id,
+    token: token.token ? token.token : undefined,
+  };
+});
 
 export const Route = createFileRoute('/_layout')({
   component: RouteComponent,
   loader: async () => {
-    console.log('loader');
-    const session = await authClient.getSession();
-    console.log('session', session);
-    return {session};
+    if (typeof window === 'undefined') {
+      console.log('getting session from server');
+      try {
+        const session = await getAuthFromHeaders();
+        console.log('got session', session);
+        return session;
+      } catch (e) {
+        if (e.statusCode === 401) {
+          console.log('user unauthenticated');
+        } else {
+          console.error(e);
+        }
+      }
+    }
+
+    return {
+      userID: undefined,
+      token: undefined,
+    };
   },
+  staleTime: Infinity,
 });
 
 function RouteComponent() {
-  const {session} = Route.useLoaderData();
+  const {userID, token} = Route.useLoaderData();
   return (
-    <SessionProvider
-      initialUserID={session.data?.user.id}
-      initialToken={session.data?.session.token}
-    >
+    <SessionProvider initialUserID={userID} initialToken={token}>
       <ZeroProvider>
         <Outlet />
       </ZeroProvider>
