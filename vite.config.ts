c0fbf2f5d09ae3ab90bde4889ceaf4184c39059a
Aaron Boodman <aaron@aaronboodman.com>
2025-06-21 23:37:21 -1000

    try again

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import {defineConfig} from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import {tanstackStart} from '@tanstack/react-start/plugin/vite';
+import {tanstackRouter} from '@tanstack/router-plugin/vite';
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tsConfigPaths(), tanstackStart({
-    target: 'vercel',
-    tsr: {
-      srcDirectory: 'app',
-    },
-  })],
+  plugins: [
+    tsConfigPaths(),
+    tanstackStart({
+      target: 'vercel',
+      tsr: {
+        srcDirectory: 'app',
+      },
+      spa: {
+        enabled: true,
+      },
+    }),
+  ],
 });
