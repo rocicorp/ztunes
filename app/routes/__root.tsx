8e3439fc94128f9d4a5757cac93e3f52c1ec2b00
Aaron Boodman <aaron@aaronboodman.com>
2025-06-26 11:32:22 -1000

    title

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -19,7 +19,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'Zero Records',
+        title: 'ztunes',
       },
     ],
   }),
