d7bd529ba4784cc48bf1dba774b5f023bf4f703b
Aaron Boodman <aaron@aaronboodman.com>
2025-12-12 14:30:14 -1000

    connection state api

---
+++ app/components/status.tsx
@@ -0,0 +1,25 @@
+import {useConnectionState} from '@rocicorp/zero/react';
+
+export function Status() {
+  const state = useConnectionState();
+  switch (state.name) {
+    case 'connecting': {
+      return <div title={state.reason}>🔄 Connecting...</div>;
+    }
+    case 'connected': {
+      return <div>✅ Connected</div>;
+    }
+    case 'disconnected': {
+      return <div title={state.reason}>🦕 Offline</div>;
+    }
+    case 'error': {
+      return <div title={state.reason}>❌ Error</div>;
+    }
+    case 'needs-auth': {
+      return <div>🔐 Session expired</div>;
+    }
+    default: {
+      throw new Error('Unexpected connection state: ' + state.name);
+    }
+  }
+}
